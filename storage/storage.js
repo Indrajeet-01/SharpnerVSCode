1dd0c2d84f2d40cd83a597ace3204526818a1681
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-04 12:43:30 +0530

    edit user

--- storage/storage.js
+++ storage/storage.js
@@ -19,12 +19,19 @@ userForm.addEventListener('submit', function(event){
         time: time,
     }
 
-    let existingUsers = JSON.parse(localStorage.getItem('userDetails')) || []
+    if(editingUserId == null){
+        let existingUsers = JSON.parse(localStorage.getItem('userDetails')) || []
     existingUsers.push(userDetails)
 
     let userDetailsJson = JSON.stringify(existingUsers)
 
     localStorage.setItem('userDetails', userDetailsJson)
+
+    } else {
+        editUser(editingUserId,userDetails)
+
+    }
+    
 
     userForm.reset()
     
@@ -37,6 +44,20 @@ function displayUserList(users) {
         users.forEach((user,index) => {
             const li = document.createElement('li')
             li.textContent = `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}, Date: ${user.date}, Time: ${user.time}`
+            // edit button
+            const edtBtn = document.createElement('button')
+            edtBtn.style.backgroundColor = 'lightgrey'
+            edtBtn.textContent = 'edit'
+            edtBtn.addEventListener('click',() => {
+                document.getElementById('name').value = user.name
+                document.getElementById('email').value = user.email
+                document.getElementById('phone').value = user.phone
+                document.getElementById('date').value = user.date
+                document.getElementById('time').value = user.time
+                editingUserId = index
+            })
+
+            // delete button
             const delBtn = document.createElement('button')
             delBtn.style.backgroundColor = 'red'
             delBtn.textContent = 'Delete'
@@ -46,12 +67,24 @@ function displayUserList(users) {
                 localStorage.setItem('userDetails',userDetailsJson)
                 
             })
+            li.appendChild(edtBtn)
             li.appendChild(delBtn)
             ul.appendChild(li)
         });
         userList.appendChild(ul)
     }
     userList.style.backgroundColor = 'white'
+}
+
+function editUser(userId, updatedUserDetails) {
+    const users = JSON.parse(localStorage.getItem('userDetails')) || [];
+    if (userId >= 0 && userId < users.length) {
+        users[userId] = updatedUserDetails;
+
+        // Update local storage with the modified array
+        const userDetailsJson = JSON.stringify(users);
+        localStorage.setItem('userDetails', userDetailsJson);
+    }
 }
 
 const storedUsers = JSON.parse(localStorage.getItem('userDetails')) || []
