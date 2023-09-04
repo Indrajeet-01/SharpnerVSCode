72a660460e4023b18b01efe703d9d98504a929c7
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-04 12:27:19 +0530

    delete user

--- storage/storage.js
+++ storage/storage.js
@@ -22,21 +22,31 @@ userForm.addEventListener('submit', function(event){
     let existingUsers = JSON.parse(localStorage.getItem('userDetails')) || []
     existingUsers.push(userDetails)
 
-    const userDetailsJson = JSON.stringify(existingUsers)
+    let userDetailsJson = JSON.stringify(existingUsers)
 
     localStorage.setItem('userDetails', userDetailsJson)
 
     userForm.reset()
-    displayUserList(existingUsers)
+    
 })
 
 function displayUserList(users) {
     
     if(users.length>0){
         const ul = document.createElement('ul')
-        users.forEach(user => {
+        users.forEach((user,index) => {
             const li = document.createElement('li')
             li.textContent = `Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}, Date: ${user.date}, Time: ${user.time}`
+            const delBtn = document.createElement('button')
+            delBtn.style.backgroundColor = 'red'
+            delBtn.textContent = 'Delete'
+            delBtn.addEventListener('click',() => {
+                users.splice(index,1)
+                let userDetailsJson = JSON.stringify(users)
+                localStorage.setItem('userDetails',userDetailsJson)
+                
+            })
+            li.appendChild(delBtn)
             ul.appendChild(li)
         });
         userList.appendChild(ul)
