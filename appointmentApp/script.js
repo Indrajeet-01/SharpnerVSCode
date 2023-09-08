440229da5d956930d28e3f3dde0c00a22a32abd2
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-08 12:28:32 +0530

    delete user

--- appointmentApp/script.js
+++ appointmentApp/script.js
@@ -14,7 +14,7 @@ userForm.addEventListener('submit', async(event)=>{
         phone: phone,
     }
 
-    const url = 'https://crudcrud.com/api/be1747d53c8245c697e2e54e5dc09acc/appointmentData'
+     const url = 'https://crudcrud.com/api/be1747d53c8245c697e2e54e5dc09acc/appointmentData'
 
     try {
         await axios.post(url, userDetails)
@@ -35,16 +35,51 @@ async function displayUserList() {
                 (user) => `
                 <li>
                 Name: ${user.name}, Email: ${user.email}, Phone: ${user.phone}
+                <button onClick=deleteUser('${user._id}')>Delete</button>
+                <button onClick=editUser('${user.name}','${user.email}','${user.phone}','${user._id}')>Edit</button>
+
                 </li>
                 `
             )
             document.getElementById('ListOfUsers').innerHTML = userDetail.join('')
-                
+            
+            
         } catch(error) {
             console.log('error in display users', error)
-
     }
     
+}
+
+async function deleteUser(userId){
+     const url = 'https://crudcrud.com/api/be1747d53c8245c697e2e54e5dc09acc/appointmentData'
+    try {
+        await axios.delete(`${url}/${userId}`)
+        
+
+    } catch (error){
+        console.log('error in delete user', error)
+
+    }
+}
+
+async function editUser(name, email, phone, userId){
+    
+    document.getElementById('name').value = name
+    document.getElementById('email').value = email
+    document.getElementById('phone').value = phone
+    const updatedUser = {
+        name,email,phone
+    }
+
+    const url = 'https://crudcrud.com/api/be1747d53c8245c697e2e54e5dc09acc/appointmentData'
+
+    try{
+        await axios.put(`${url}/${userId}`,updatedUser)
+
+    } catch (error) {
+        console.log('error in delete user', error)
+    }
+
 }
 
 displayUserList()
