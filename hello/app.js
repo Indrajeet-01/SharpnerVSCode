4a23292179fdeb489e1ce2254fecf7bebdc9bbdb
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-13 11:32:14 +0530

    write in file

--- hello/app.js
+++ hello/app.js
@@ -1,17 +1,25 @@
 var http = require('http');
+var fs = require('fs')
 //create a server object:
 
 http.createServer(function (req, res) {
     const url = req.url
+    const method = req.method
     res.statusCode = 200
-    if(url == '/home')
-        res.end('welcome home')
-    else if(url == '/about')
-        res.end('welcome to about us')
-    else if(url == '/node')
-        res.end('welcome node')
-    else 
-        res.end('welcome here')
+    if(url == '/'){
+        res.write('<html>')
+        res.write('<head><title> Enter message </title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    if(url == '/message' && method == 'POST'){
+        fs.writeFileSync('message.txt', 'DUMMY')
+        res.statusCode = 302
+        res.setHeader('Location' , '/')
+        res.end()
+    }
+    
     
 }).listen(4000); 
 
