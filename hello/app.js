048359726a88bc7409b9fb9a758ab21efbd181aa
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-13 11:14:26 +0530

    req res

--- hello/app.js
+++ hello/app.js
@@ -2,8 +2,17 @@ var http = require('http');
 //create a server object:
 
 http.createServer(function (req, res) {
-    res.write('Indrajeet'); 
-    res.end(); 
+    const url = req.url
+    res.statusCode = 200
+    if(url == '/home')
+        res.end('welcome home')
+    else if(url == '/about')
+        res.end('welcome to about us')
+    else if(url == '/node')
+        res.end('welcome node')
+    else 
+        res.end('welcome here')
+    
 }).listen(4000); 
 
 console.log('Server running at 4000');
