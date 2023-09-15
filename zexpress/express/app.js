634815aafe445a077d4f88e960c9d8aa11f7c3db
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-15 11:19:16 +0530

    cart relation

--- zexpress/express/app.js
+++ zexpress/express/app.js
@@ -4,6 +4,8 @@ const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 const User = require('./model/user')
 const Product = require('./model/product')
+const Cart = require('./model/cart')
+const CartItem = require('./model/cart-item')
 
 const errorController = require('../controller/index')
 
@@ -16,6 +18,10 @@ app.use(shopRoute)
 app.use(errorController.get404)
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
-User.hasMany(Products)
+User.hasMany(Product)
+User.hasOne(Cart)
+Cart.belongsTo(User)
+Cart.belongsToMany(Product, {through: CartItem})
+Product.belongsToMany(Cart, {through: CartItem})
 
 app.listen(3000)
