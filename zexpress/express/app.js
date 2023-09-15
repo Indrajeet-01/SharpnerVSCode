424cc3bc4e246d792e66a154b6adbd390fb6ece8
Indrajeet Singh <indrajeetsinghyadav4@gmail.com>
2023-09-15 11:07:01 +0530

    make relations

--- zexpress/express/app.js
+++ zexpress/express/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
+const User = require('./model/user')
+const Product = require('./model/product')
 
 const errorController = require('../controller/index')
 
@@ -12,5 +14,8 @@ app.use('/admin', adminRoute)
 app.use(shopRoute)
 
 app.use(errorController.get404)
+
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'})
+User.hasMany(Products)
 
 app.listen(3000)
